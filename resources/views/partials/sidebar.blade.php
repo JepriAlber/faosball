8c2803aea5e1167172636fd2cb6fd2a75b384b17
JepriAlber <jeprialber@gmail.com>
2026-07-16 21:00:57 +0700

    refactor: rapikan menu sidebar dan hapus scaffold profil-test
    - Heading "Administration" dipindah ke dalam blok @if isSuperAdmin(),
      sebelumnya heading tetap tampil untuk non-super-admin walau
      submenu-nya sendiri sudah disembunyikan.
    - Menu item "Profile" dipindah ke bawah grup "Football Academy" biar
      urutannya lebih logis, dan formatting class Blade dirapikan.
    - Hapus resources/views/profil-test/ (scaffold dashboard demo dari
      Breeze/TailAdmin yang tidak pernah dipakai).

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -31,9 +31,8 @@
                         MENU
                     </span>
                     {{-- Dots icon saat sidebar collapsed di desktop --}}
-                    <svg :class="sidebarToggle ? 'lg:block hidden' : 'hidden'"
-                        class="menu-group-icon" width="24" height="24" viewBox="0 0 24 24"
-                        fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg :class="sidebarToggle ? 'lg:block hidden' : 'hidden'" class="menu-group-icon" width="24"
+                        height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" clip-rule="evenodd"
                             d="M5.99915 10.2451C6.96564 10.2451 7.74915 11.0286 7.74915 11.9951V12.0051C7.74915 12.9716 6.96564 13.7551 5.99915 13.7551C5.03265 13.7551 4.24915 12.9716 4.24915 12.0051V11.9951C4.24915 11.0286 5.03265 10.2451 5.99915 10.2451ZM17.9991 10.2451C18.9656 10.2451 19.7491 11.0286 19.7491 11.9951V12.0051C19.7491 12.9716 18.9656 13.7551 17.9991 13.7551C17.0326 13.7551 16.2491 12.9716 16.2491 12.0051V11.9951C16.2491 11.0286 17.0326 10.2451 17.9991 10.2451ZM13.7491 11.9951C13.7491 11.0286 12.9656 10.2451 11.9991 10.2451C11.0326 10.2451 10.2491 11.0286 10.2491 11.9951V12.0051C10.2491 12.9716 11.0326 13.7551 11.9991 13.7551C12.9656 13.7551 13.7491 12.9716 13.7491 12.0051V11.9951Z"
                             fill="" />
@@ -75,8 +74,7 @@
 
                         {{-- Dropdown sub-menu --}}
                         <div x-show="open" x-collapse class="overflow-hidden">
-                            <ul :class="sidebarToggle ? 'lg:hidden' : 'flex'"
-                                class="menu-dropdown">
+                            <ul :class="sidebarToggle ? 'lg:hidden' : 'flex'" class="menu-dropdown">
                                 <li>
                                     <a href="{{ route('dashboard') }}" class="menu-dropdown-item group"
                                         :class="{{ Route::is('dashboard') ? 'true' : 'false' }} ? 'menu-dropdown-item-active' :
@@ -89,37 +87,15 @@
                     </li>
                     <!-- ===== END: Dashboard ===== -->
 
-                    <!-- ===== Menu Item: Profile (tanpa dropdown) ===== -->
-                    @php
-                        $isProfileActive = Route::is('profile');
-                    @endphp
-
-                    <li>
-                        <a href="#"
-                            class="menu-item group {{ $isProfileActive ? 'menu-item-active' : 'menu-item-inactive' }}">
-                            <svg class="{{ $isProfileActive ? 'menu-item-icon-active' : 'menu-item-icon-inactive' }}"
-                                width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M12 3.5C7.30558 3.5 3.5 7.30558 3.5 12C3.5 14.1526 4.3002 16.1184 5.61936 17.616C6.17279 15.3096 8.24852 13.5955 10.7246 13.5955H13.2746C15.7509 13.5955 17.8268 15.31 18.38 17.6167C19.6996 16.119 20.5 14.153 20.5 12C20.5 7.30558 16.6944 3.5 12 3.5ZM17.0246 18.8566V18.8455C17.0246 16.7744 15.3457 15.0955 13.2746 15.0955H10.7246C8.65354 15.0955 6.97461 16.7744 6.97461 18.8455V18.856C8.38223 19.8895 10.1198 20.5 12 20.5C13.8798 20.5 15.6171 19.8898 17.0246 18.8566ZM2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM11.9991 7.25C10.8847 7.25 9.98126 8.15342 9.98126 9.26784C9.98126 10.3823 10.8847 11.2857 11.9991 11.2857C13.1135 11.2857 14.0169 10.3823 14.0169 9.26784C14.0169 8.15342 13.1135 7.25 11.9991 7.25ZM8.48126 9.26784C8.48126 7.32499 10.0563 5.75 11.9991 5.75C13.9419 5.75 15.5169 7.32499 15.5169 9.26784C15.5169 11.2107 13.9419 12.7857 11.9991 12.7857C10.0563 12.7857 8.48126 11.2107 8.48126 9.26784Z"
-                                    fill="" />
-                            </svg>
-
-                            <span class="menu-item-text" :class="sidebarToggle ? 'lg:hidden' : ''">
-                                User Profile
-                            </span>
-                        </a>
-                    </li>
-                    <!-- ===== END: Profile ===== -->
 
                     <h3 class="menu-group-heading">
                         <span class="menu-group-title" :class="sidebarToggle ? 'lg:hidden' : ''">
                             football academy
                         </span>
                         {{-- Dots icon saat sidebar collapsed di desktop --}}
-                        <svg :class="sidebarToggle ? 'lg:block hidden' : 'hidden'"
-                            class="menu-group-icon" width="24" height="24"
-                            viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg :class="sidebarToggle ? 'lg:block hidden' : 'hidden'" class="menu-group-icon"
+                            width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" clip-rule="evenodd"
                                 d="M5.99915 10.2451C6.96564 10.2451 7.74915 11.0286 7.74915 11.9951V12.0051C7.74915 12.9716 6.96564 13.7551 5.99915 13.7551C5.03265 13.7551 4.24915 12.9716 4.24915 12.0051V11.9951C4.24915 11.0286 5.03265 10.2451 5.99915 10.2451ZM17.9991 10.2451C18.9656 10.2451 19.7491 11.0286 19.7491 11.9951V12.0051C19.7491 12.9716 18.9656 13.7551 17.9991 13.7551C17.0326 13.7551 16.2491 12.9716 16.2491 12.0051V11.9951C16.2491 11.0286 17.0326 10.2451 17.9991 10.2451ZM13.7491 11.9951C13.7491 11.0286 12.9656 10.2451 11.9991 10.2451C11.0326 10.2451 10.2491 11.0286 10.2491 11.9951V12.0051C10.2491 12.9716 11.0326 13.7551 11.9991 13.7551C12.9656 13.7551 13.7491 12.9716 13.7491 12.0051V11.9951Z"
                                 fill="" />
@@ -175,8 +151,7 @@
 
                         <div x-show="open" x-collapse class="overflow-hidden">
 
-                            <ul :class="sidebarToggle ? 'lg:hidden' : 'flex'"
-                                class="menu-dropdown">
+                            <ul :class="sidebarToggle ? 'lg:hidden' : 'flex'" class="menu-dropdown">
                                 {{-- Players --}}
                                 <li>
                                     <a href="{{ route('players.index') }}" class="menu-dropdown-item group"
@@ -207,22 +182,46 @@
                     <!-- ===== END: Football Academy ===== -->
 
 
-                    <h3 class="menu-group-heading">
-                        <span class="menu-group-title" :class="sidebarToggle ? 'lg:hidden' : ''">
-                            Administration
-                        </span>
-                        {{-- Dots icon saat sidebar collapsed di desktop --}}
-                        <svg :class="sidebarToggle ? 'lg:block hidden' : 'hidden'"
-                            class="menu-group-icon" width="24" height="24"
-                            viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd"
-                                d="M5.99915 10.2451C6.96564 10.2451 7.74915 11.0286 7.74915 11.9951V12.0051C7.74915 12.9716 6.96564 13.7551 5.99915 13.7551C5.03265 13.7551 4.24915 12.9716 4.24915 12.0051V11.9951C4.24915 11.0286 5.03265 10.2451 5.99915 10.2451ZM17.9991 10.2451C18.9656 10.2451 19.7491 11.0286 19.7491 11.9951V12.0051C19.7491 12.9716 18.9656 13.7551 17.9991 13.7551C17.0326 13.7551 16.2491 12.9716 16.2491 12.0051V11.9951C16.2491 11.0286 17.0326 10.2451 17.9991 10.2451ZM13.7491 11.9951C13.7491 11.0286 12.9656 10.2451 11.9991 10.2451C11.0326 10.2451 10.2491 11.0286 10.2491 11.9951V12.0051C10.2491 12.9716 11.0326 13.7551 11.9991 13.7551C12.9656 13.7551 13.7491 12.9716 13.7491 12.0051V11.9951Z"
-                                fill="" />
-                        </svg>
-                    </h3>
+                    <!-- ===== Menu Item: Profile (tanpa dropdown) ===== -->
+                    @php
+                        $isProfileActive = Route::is('profile');
+                    @endphp
+
+                    <li>
+                        <a href="#"
+                            class="menu-item group {{ $isProfileActive ? 'menu-item-active' : 'menu-item-inactive' }}">
+                            <svg class="{{ $isProfileActive ? 'menu-item-icon-active' : 'menu-item-icon-inactive' }}"
+                                width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M12 3.5C7.30558 3.5 3.5 7.30558 3.5 12C3.5 14.1526 4.3002 16.1184 5.61936 17.616C6.17279 15.3096 8.24852 13.5955 10.7246 13.5955H13.2746C15.7509 13.5955 17.8268 15.31 18.38 17.6167C19.6996 16.119 20.5 14.153 20.5 12C20.5 7.30558 16.6944 3.5 12 3.5ZM17.0246 18.8566V18.8455C17.0246 16.7744 15.3457 15.0955 13.2746 15.0955H10.7246C8.65354 15.0955 6.97461 16.7744 6.97461 18.8455V18.856C8.38223 19.8895 10.1198 20.5 12 20.5C13.8798 20.5 15.6171 19.8898 17.0246 18.8566ZM2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM11.9991 7.25C10.8847 7.25 9.98126 8.15342 9.98126 9.26784C9.98126 10.3823 10.8847 11.2857 11.9991 11.2857C13.1135 11.2857 14.0169 10.3823 14.0169 9.26784C14.0169 8.15342 13.1135 7.25 11.9991 7.25ZM8.48126 9.26784C8.48126 7.32499 10.0563 5.75 11.9991 5.75C13.9419 5.75 15.5169 7.32499 15.5169 9.26784C15.5169 11.2107 13.9419 12.7857 11.9991 12.7857C10.0563 12.7857 8.48126 11.2107 8.48126 9.26784Z"
+                                    fill="" />
+                            </svg>
+
+                            <span class="menu-item-text" :class="sidebarToggle ? 'lg:hidden' : ''">
+                                Profile
+                            </span>
+                        </a>
+                    </li>
+                    <!-- ===== END: Profile ===== -->
+
+
 
                     {{-- ===== Administration ===== --}}
                     @if (app(\App\Services\AcademyService::class)->isSuperAdmin())
+                        <h3 class="menu-group-heading">
+                            <span class="menu-group-title" :class="sidebarToggle ? 'lg:hidden' : ''">
+                                Administration
+                            </span>
+                            {{-- Dots icon saat sidebar collapsed di desktop --}}
+                            <svg :class="sidebarToggle ? 'lg:block hidden' : 'hidden'" class="menu-group-icon"
+                                width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M5.99915 10.2451C6.96564 10.2451 7.74915 11.0286 7.74915 11.9951V12.0051C7.74915 12.9716 6.96564 13.7551 5.99915 13.7551C5.03265 13.7551 4.24915 12.9716 4.24915 12.0051V11.9951C4.24915 11.0286 5.03265 10.2451 5.99915 10.2451ZM17.9991 10.2451C18.9656 10.2451 19.7491 11.0286 19.7491 11.9951V12.0051C19.7491 12.9716 18.9656 13.7551 17.9991 13.7551C17.0326 13.7551 16.2491 12.9716 16.2491 12.0051V11.9951C16.2491 11.0286 17.0326 10.2451 17.9991 10.2451ZM13.7491 11.9951C13.7491 11.0286 12.9656 10.2451 11.9991 10.2451C11.0326 10.2451 10.2491 11.0286 10.2491 11.9951V12.0051C10.2491 12.9716 11.0326 13.7551 11.9991 13.7551C12.9656 13.7551 13.7491 12.9716 13.7491 12.0051V11.9951Z"
+                                    fill="" />
+                            </svg>
+                        </h3>
                         @php
                             $administrationRoutes = ['roles.*', 'permissions.*', 'academies.*'];
 
@@ -263,8 +262,7 @@
 
                             <div x-show="open" x-collapse class="overflow-hidden">
 
-                                <ul :class="sidebarToggle ? 'lg:hidden' : 'flex'"
-                                    class="menu-dropdown">
+                                <ul :class="sidebarToggle ? 'lg:hidden' : 'flex'" class="menu-dropdown">
                                     {{-- Roles --}}
                                     <li>
                                         <a href="{{ route('roles.index') }}" class="menu-dropdown-item group"
